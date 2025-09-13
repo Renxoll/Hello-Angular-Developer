d7d413eca92848e94c0c20a1436df5a1171c79df
Renxoll <112541849+Renxoll@users.noreply.github.com>
2025-09-13 17:06:27 -0500

    update app.html and app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  DeveloperRegistrationComponent
+} from './greetings/presentation/components/developer-registration/developer-registration.component';
+import {
+  DeveloperGreetingComponent
+} from './greetings/presentation/components/developer-greeting/developer-greeting.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DeveloperRegistrationComponent, DeveloperGreetingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hello-angng g clular-developer';
+  public firstName: string = '';
+  public lastName: string = '';
+
+  public updateRegisteredDeveloperInfo(developer:{firstName: string; lastName: string }): void {
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
+
+  public resetRegisteredDeveloperInfo() {
+    this.firstName = '';
+    this.lastName = '';
+  }
 }
