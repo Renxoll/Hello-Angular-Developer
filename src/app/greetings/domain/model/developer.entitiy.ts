0f7b9b165746e8460bfc6b283227c1c833096fbe
Renxoll <112541849+Renxoll@users.noreply.github.com>
2025-09-13 16:05:40 -0500

    add developer entity

---
+++ src/app/greetings/domain/model/developer.entitiy.ts
@@ -0,0 +1,23 @@
+export class Developer {
+  private readonly _firstName: string;
+  private readonly _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get fullName(): string {
+    return `${this._firstName} ${this._lastName}`;
+  }
+}
+
+
